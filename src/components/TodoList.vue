417935f413247b30b5e27959f548afbf51a6596d
thejesusof <thejesusof@gmail.com>
2018-05-30 12:02:40 +0300

    TodoList component created

---
+++ src/components/TodoList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ul>
+        <li> Todo-to-to-do </li>
+        <li> Todo-to-to-do </li>
+        <li> Todo-to-to-do </li>
+    </ul>
+  </div>
+</template>
+
+<script type = "text/javascript">
+
+export default {
+};
+</script>
+<style>
+</style>
